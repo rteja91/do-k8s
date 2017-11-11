953e1c1c6c85a74fc743a75e3f656ce4b3897485
raviteja <rteja7228@gmail.com>
2017-11-11 15:07:30 +0530

    updated config for testing

--- master.sh
+++ master.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Replace this with the token 
-TOKEN=xxxxxx.yyyyyyyyyyyyyyyy
+TOKEN=oc6q8t.62x8d8a4mkeeww0n
 
 apt-get update && apt-get upgrade -y 
 
@@ -15,14 +15,14 @@ apt-get update -y
 apt-get install -y docker.io
 apt-get install -y --allow-unauthenticated kubelet kubeadm kubectl kubernetes-cni
 
-export MASTER_IP=$(curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address)
+export MASTER_IP=$(curl -s https://api.ipify.org)
 kubeadm init --pod-network-cidr=10.244.0.0/16  --apiserver-advertise-address $MASTER_IP --token $TOKEN
 
 cp /etc/kubernetes/admin.conf $HOME/
 chown $(id -u):$(id -g) $HOME/admin.conf
 export KUBECONFIG=$HOME/admin.conf
 
-kubectl create -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel-rbac.yml --namespace=kube-system
+kubectl create -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/k8s-manifests/kube-flannel-rbac.yml --namespace=kube-system
 kubectl create -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml --namespace=kube-system
-kubectl create -f https://rawgit.com/kubernetes/dashboard/master/src/deploy/kubernetes-dashboard.yaml --namespace=kube-system
+kubectl create -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml --namespace=kube-system
 
